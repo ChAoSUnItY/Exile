702eb36eea21ac430e72a0ca7e2e009f54c74319
KyleLin921021 <43753315+KyleLin921021@users.noreply.github.com>
2021-11-09 00:24:57 +0800

    make lexing rule strict (one definition per line) - make lex process parallel

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,32 +1,32 @@
-use std::mem::ManuallyDrop;
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Token {
     Ok(Type, String),
     Err(String),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Type {
     Identifier,
+    Whitespace,
+    LineBreak,
     Colon,
 }
 
 pub fn lex(src: String) -> Vec<Token> {
     let len = src.len();
     let mut pos: usize = 0;
-    let mut chars = src.chars();
+    let chars = src.chars().collect::<Vec<char>>();
     let mut tokens = Vec::<Token>::new();
 
-    while pos < len {
-        let opt_character = chars.next();
+    while pos <= len {
+        let opt_character = chars.get(pos);
 
         if let Some(character) = opt_character {
-            tokens.push(if character.is_alphabetic() {
+            let token = if character.is_alphabetic() {
                 let start = pos;
     
                 loop {
-                    if let Some(c) = chars.next() {
+                    if let Some(c) = chars.get(pos) {
                         if c.is_alphanumeric() {
                             pos += 1;
                             continue;
@@ -37,18 +37,22 @@ pub fn lex(src: String) -> Vec<Token> {
 
                 Token::Ok(
                     Type::Identifier,
-                    src[start..=pos].to_string(),
+                    src[start..pos].to_string(),
                 )
             } else {
                 let token = match character {
                     ':' => Token::Ok(Type::Colon, character.to_string()),
+                    character if character.is_whitespace() => Token::Ok(Type::Whitespace, character.to_string()),
+                    '\r' | '\n' => Token::Ok(Type::LineBreak, character.to_string()),
                     _ => Token::Err(character.to_string()),
                 };
 
                 pos += 1;
                 
                 token
-            });
+            };
+
+            tokens.push(token);
         } else {
             break;
         }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
-use std::{fs::File, io::{self, BufRead, BufReader}};
-
 mod lexer;
 
-fn main() {
-    if let Ok(file) = File::open("./examples/e1.exile") {
-        let reader = BufReader::new(file);
+extern crate rayon;
 
-        for result in reader.lines() {
-            if let Ok(line) = result {
-                println!("{:?}", lexer::lex(line));
-            }
+use lexer::{lex, Token};
+use rayon::prelude::*;
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader, Error},
+    sync::{Arc, Mutex},
+};
+
+fn main() {
+    let file_name = "./examples/e1.exile";
+
+    if let Ok(file) = File::open(file_name) {
+        let reader = BufReader::new(file);
+        let tokens = Arc::new(Mutex::new(Vec::<Token>::new()));
+        let line_vec = reader.lines().collect::<Vec<Result<String, Error>>>();
+
+        if line_vec.par_iter().any(|result| result.is_err()) {
+            eprintln!("Unable to read file {}", file_name);
+            return;
         }
+
+        line_vec.par_iter().for_each(|r| {
+            tokens
+                .lock()
+                .unwrap()
+                .append(&mut lex(r.as_ref().unwrap().to_string()));
+        });
+
+        println!(
+            "{:?}",
+            tokens
+        );
     }
 }
